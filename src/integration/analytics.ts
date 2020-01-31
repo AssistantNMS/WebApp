dbdcd946baaf0a82a3a8f82dff13581a66df7196
Kurt Lourens <kurt.lourens@outlook.com>
2020-01-31 14:31:10 +0200

    fix analytics

--- src/integration/analytics.ts
+++ src/integration/analytics.ts
@@ -1,14 +1,14 @@
 import ReactGA from 'react-ga';
 
 export const initAnalytics = () => {
-    if (window.config.googleAnalyticsEnabled) return;
+    if (!window.config.googleAnalyticsEnabled) return;
 
     ReactGA.initialize('UA-145632237-3');
     ReactGA.pageview(window.location.pathname + window.location.search);
 }
 
 export const trackEvent = (analyticsEvent: string) => {
-    if (window.config.googleAnalyticsEnabled) return;
+    if (!window.config.googleAnalyticsEnabled) return;
 
     ReactGA.event({
         category: analyticsEvent,
