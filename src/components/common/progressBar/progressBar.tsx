8d6b23ed92729c313d0a0ac50174862b081ee790
Kurt Lourens <kurt.lourens@outlook.com>
2020-05-08 14:17:51 +0200

    center align progress bar

--- src/components/common/progressBar/progressBar.tsx
+++ src/components/common/progressBar/progressBar.tsx
@@ -7,12 +7,12 @@ interface IProps {
 export const ProgressBar = (props: IProps) => {
     const percentageString = `${props.percentage}%`;
     return (
-        <div className="progress custom">
+        <div className="progress custom" style={{ textAlign: 'center' }}>
             <div className="progress-bar" role="progressbar"
                 aria-valuenow={props.percentage} aria-valuemin={0} aria-valuemax={100}
                 style={{ width: percentageString }}>
-                <span className="text">{percentageString}</span>
             </div>
+            <span className="text">{percentageString}</span>
         </div>
     )
 }
