9a0ff5160a1150b917481de10577a6083d8d469f
Shikhar Sharma <ceoshikhar@gmail.com>
2020-05-30 04:01:57 +0530

    Create errorComponent. Formatted code in some files. Used the errorComponent in processorPresenter.

---
+++ src/components/core/errorComponent/errorComponent.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+
+export const Error = () => {
+    return (
+        <>
+            <div className="content">
+                <h1>ERROR</h1>          
+                <h2 className="largeHeading">Oops... something went wrong!</h2>
+            </div>
+        </>
+    )
+}
