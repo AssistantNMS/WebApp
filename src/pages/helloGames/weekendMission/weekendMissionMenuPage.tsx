bb2cd77ad996284d68860d52861fd7ae1e01091e
Kurt Lourens <kurt.lourens@outlook.com>
2021-05-27 19:59:23 +0200

    remove unused weekendMission files

--- src/pages/helloGames/weekendMission/weekendMissionMenuPage.tsx
+++ src/pages/helloGames/weekendMission/weekendMissionMenuPage.tsx
@@ -16,15 +16,6 @@ interface IProps {
 
 const WeekendMissionMenuPresenterUnconnected: React.FC<IProps> = (props: IProps) => {
 
-    const seasonComingSoon = {
-        isDisabled: true,
-        // weekendMissionJson: LocaleKey.weekendMissionSeason3Json,
-        // season: 'MP_PORTALQUEST',
-        // level: 30,
-        // maxLevel: 30,
-        // minLevel: 1,
-    };
-
     const season2Obj = {
         weekendMissionJson: LocaleKey.weekendMissionSeason2Json,
         season: 'MP_PORTALQUEST',
@@ -46,7 +37,7 @@ const WeekendMissionMenuPresenterUnconnected: React.FC<IProps> = (props: IProps)
 
         if (external != null) {
             return (
-                <a href={external} target="_blank" rel="noopener noreferrer" className={classNames('col-12 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-cs-12 weekend-mission-card')} style={{ backgroundImage: `url(/assets/images/${imageName})` }}>
+                <a href={external} target="_blank" rel="noopener noreferrer" className={classNames('col-12 col-xl-6 col-lg-12 col-md-12 col-sm-12 col-cs-12 weekend-mission-card')} style={{ backgroundImage: `url(/assets/images/${imageName})` }}>
                     <p className="mt-2 mr-1 noselect">{credits}</p>
                     <h3 className="noselect bottom-text">{title}</h3>
                 </a>
@@ -54,7 +45,7 @@ const WeekendMissionMenuPresenterUnconnected: React.FC<IProps> = (props: IProps)
         };
 
         return (
-            <div className={classNames('col-12 col-xl-6 col-lg-6 col-md-12 col-sm-12 col-cs-12 weekend-mission-card', { pointer: !state.isDisabled })} style={{ backgroundImage: `url(/assets/images/${imageName})` }}
+            <div className={classNames('col-12 col-xl-6 col-lg-12 col-md-12 col-sm-12 col-cs-12 weekend-mission-card', { pointer: !state.isDisabled })} style={{ backgroundImage: `url(/assets/images/${imageName})` }}
                 onClick={() => {
                     if (state.isDisabled) return;
                     props.history.push({
@@ -75,7 +66,6 @@ const WeekendMissionMenuPresenterUnconnected: React.FC<IProps> = (props: IProps)
             <div className="content">
                 <div className="container full pt1">
                     <div className="row weekend-mission">
-                        {weekendMissionCard('Season 3', 'special/underConstruction.svg', '', seasonComingSoon)}
                         {weekendMissionCard('Season 2', 'weekendMission/Season2.jpg', 'stoz0r', season2Obj)}
                         {weekendMissionCard('Season 1', 'weekendMission/Season1.jpg', 'screenshot guy', season1Obj)}
                         {weekendMissionCard('NMS Wiki', 'weekendMission/wiki.jpg', 'cyberpunk2350', {}, ExternalUrls.nmsWeekendMissionWiki)}
