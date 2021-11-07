415a45b072cbd70912baa37a90c87ca07d344c32
Kurt Lourens <kurt.lourens@outlook.com>
2021-11-07 22:10:25 +0100

    regen with fewer images

--- src/components/common/tile/imageContainer.tsx
+++ src/components/common/tile/imageContainer.tsx
@@ -24,11 +24,17 @@ export const ImageContainer = (props: IProps) => {
     }
     return (
         <div className="image-container" style={styleObj}>
+            {/* <img
+                src={imageString}
+                alt={props.Name ?? props.Icon}
+                draggable={false}
+            /> */}
             <LazyLoadImage
                 src={imageString}
                 className={classNames({ grayscale: props.greyScale === true })}
-                alt={props.Description ?? props.Name}
-                draggable={false} />
+                alt={props.Name ?? props.Icon}
+                draggable={false}
+            />
             {
                 (props.OutputQuantity != null && props.OutputQuantity > 0)
                     ? <BadgeContainer Quantity={props.OutputQuantity} Colour={props.BadgeColour} />
