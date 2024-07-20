ba9dd40c8645921390d255fb525e7e6cc000a78c
Lenni009 <guentherhanslphdyt@gmail.com>
2024-07-20 16:50:42 +0200

    formatting

--- src/components/core/itemHeaderRow.tsx
+++ src/components/core/itemHeaderRow.tsx
@@ -57,7 +57,7 @@ const ItemHeaderRowUnconnected: React.FC<IProps> = (props: IProps) => {
                     <i className="material-icons pointer"
                         onClick={props.openDevProperties}
                         style={{ position: 'absolute', top: '.5em', left: '.5em', color: invertColor(props.Colour || '000000', true) }}
-						title="Developer Details">code</i>
+                        title="Developer Details">code</i>
                 }
             </div>
             <div className="col-12 col-lg-10 col-md-10 col-sm-8 col-xs-9 ta-left ta-center-sm">
